ea524939315619659168a3e4d2d6f82e16347bd1
Gustavo Prado <gustavo.doprado99@outlook.com>
2022-05-01 19:03:27 -0300

    Add project files.

---
+++ ByteBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            Funcionario pedro = new Funcionario();
+            pedro.Nome = "Pedro";
+            pedro.CPF = "524.458.645-48";
+            pedro.Salario = 2000;
+
+            Diretor leticia = new Diretor();
+            leticia.Nome = "Letícia";
+            leticia.CPF = "562.487.465-89";
+            leticia.Salario = 4000;
+
+            pedro.GetBonificacao();
+            leticia.GetBonificacao();
+
+            gerenciador.Registrar(pedro);
+            gerenciador.Registrar(leticia);
+            Console.WriteLine(gerenciador.GetTotalBonificacao());
+
+            Console.ReadKey();
+        }
+    }
+}
